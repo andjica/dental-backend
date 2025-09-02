4a42156bdd44c0f44e65e04f440ac5e42f67d5ee
Andjica <andjaaa95@gmail.com>
2025-09-02 16:55:10 +0200

    new email

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -8,18 +8,10 @@ use Illuminate\Notifications\Messages\MailMessage;
 
 class AuthServiceProvider extends ServiceProvider
 {
-    /**
-     * The policy mappings for the application.
-     *
-     * @var array<class-string, class-string>
-     */
     protected $policies = [
         // 'App\Models\Model' => 'App\Policies\ModelPolicy',
     ];
 
-    /**
-     * Register any authentication / authorization services.
-     */
     public function boot(): void
     {
         $this->registerPolicies();
@@ -27,17 +19,14 @@ class AuthServiceProvider extends ServiceProvider
         VerifyEmail::toMailUsing(function ($notifiable, $url) {
             $frontendUrl = config('app.frontend_url') ?? env('FRONTEND_URL');
 
-            // parsiraj query parametre iz originalnog $url
-            $parsed = parse_url($url);
-            parse_str($parsed['query'], $query);
-
-            // generiši frontend link
-            $verificationUrl = $frontendUrl . '/verify-email?' . http_build_query($query);
+            // umesto kompletnog query stringa -> vodi na success rutu sa emailom
+            $verificationUrl = $frontendUrl . '/verify-success?email=' . urlencode($notifiable->getEmailForVerification());
 
             return (new MailMessage)
-                ->subject('Verify Email Address - andjela')
+                ->subject('Verify Email Address')
                 ->line('Please click the button below to verify your email address.')
                 ->action('Verify Email Address', $verificationUrl);
         });
     }
 }
+
